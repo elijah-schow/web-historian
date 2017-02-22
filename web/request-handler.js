56fa6283b065e83fcbbdccfa21c56826af2860e6
elijah-schow <elijah.schow@gmail.com>
2017-02-22 17:30:08 -0600

    Begin implementing router

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,23 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var url = require('url');
+var fs = require('fs');
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handleRequest = function (request, response) {
+  var path = url.parse(request.url).pathname;
+  if (path === '/' && request.method === 'GET') {
+    fs.readFile('./web/public/index.html', function(error, data) {
+      if (error) {
+        console.log('index.html failed to load\n', error);
+        response.statusCode = 500;
+        response.end();
+      } else {
+        response.statusCode = 200;
+        response.end(data);
+      }
+    });
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
 };
