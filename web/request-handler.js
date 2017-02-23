5d6d063aa06cfea7c124ba7c4be9a26740a6c7a8
Elijah Schow <elijah.schow@gmail.com>
2017-02-23 12:11:24 -0600

    Update dependancies and re-organize files

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,7 +3,7 @@ var archive = require('../helpers/archive-helpers');
 var url = require('url');
 var fs = require('fs');
 var qs = require('qs');
-var helpers = require('./http-helpers.js');
+var helpers = require('../helpers/http-helpers.js');
 
 exports.handleRequest = function (request, response) {
   var path = url.parse(request.url).pathname;
